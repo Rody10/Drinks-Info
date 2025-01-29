bf570f0743442dab88966670f9bfc04e18e3f93c
Rody10 <rodneysimbatholanah@gmail.com>
2025-01-29 18:19:16 +0200

    first commit

---
+++ Drinks-Info/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+
+
+namespace Drinks_Info
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(
+            //   new MediaTypeWithQualityHeaderValue(""))
+            await ProcessDrinksAsync(client);     
+
+        }
+
+        static async Task ProcessDrinksAsync(HttpClient client)
+        {
+            var json = await client.GetStringAsync(
+                "https://www.thecocktaildb.com/api/json/v1/1/random.php");
+            Console.WriteLine(json);
+        }
+    }
+}
